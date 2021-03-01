4791b81177fdcdd60b8c5908f54e67511e562db0
Ian Marshall <imarshall14@gmail.com>
2021-03-01 17:06:34 -0500

    commit tree fa68e4ec4fa9e6432c03c84ee9505e32d2239c28

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,13 +25,12 @@ class SongsController < ApplicationController
   end
 
   def new
-    if params[:artist_id] && !Artist.exists?(params[:author_id])
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
       redirect_to artists_path, alert:"Artist not found."
     else
       @song = Song.new(artist_id: params[:artist_id])
       @artist = Artist.find_by(id: params[:artist_id])
     end
-
   end
 
   def create
